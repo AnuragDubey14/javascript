d15ade54b235da3e33980ee31bc17c4d6111f556
Anurag Dubey <129617718+AnuragDubey14@users.noreply.github.com>
2025-05-14 06:10:17 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive data type
+
+// 7 types (call by value)
+// string, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference (Non Primitive) type
+
+// Arrays, Objects, Functions
+
+// javascript dynamically typed language
+
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId)
+
+const bigNumber = 1445310n
+
+
+const heroes=["shaktiman","naagraj","doga"]
+let myObj={
+    name:"anurag",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("hello world!")
+}
+
+console.log(typeof(myObj))
+console.log(myObj.age)
